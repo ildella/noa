8bc876b03b2d23967926193fcf7f887bdb88fdd2
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2024-12-31 20:43:19 -0300

    using new version notation for pre-release, both in package.json and cargo.toml. New bumo-version script almost ready an with some changes in commit already and precious --no-confirm added to cargo release as well, removing the annoying confirmation request. Also yarn 4.6.0 insalled via corepack

--- bin/version-bump.sh
+++ bin/version-bump.sh
@@ -13,14 +13,15 @@ git push --tags
 
 echo "Update to version $target"
 
-cargo release "$target" --manifest-path src-tauri/Cargo.toml --no-publish --no-tag --no-push --execute
+# ?? cargo release beta --manifest-path src-tauri/Cargo.toml --no-publish --no-tag --no-push --no-confirm --execute
+cargo release "$target" --manifest-path src-tauri/Cargo.toml --no-publish --no-tag --no-push --no-confirm --execute
+# ?? yarn version prerelease
 yarn version "$target"
-git commit -am "Bump version to $target"
-git push
-
-echo "Completed."
 
 NEW_NODE_VERSION=$(jq -r .version package.json)
 NEWCARGO_VERSION=$(grep '^version =' src-tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
+
+git commit -am "New development cycle. Node $NEW_NODE_VERSION | Cargo $NEWCARGO_VERSION"
+git push
 
 echo "New version: $NEWCARGO_VERSION - $NEW_NODE_VERSION"
