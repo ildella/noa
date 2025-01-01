044ef512109eba4a516d3c746fb5b19f6814c373
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-01-01 19:47:44 -0300

    remove the dev desktop handler

--- cicd/debug-install.sh
+++ cicd/debug-install.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+# This handler is installed by `yarn tauri dev`. 
+# Removed to avoid collision with the installed debug version. 
+rm ~/.local/share/applications/NOA-handler.desktop
+
 VERSION=$(grep '^version =' src-tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
 yarn desktop.build.deb -d
 sudo dpkg -i src-tauri/target/debug/bundle/deb/NOA_"$VERSION"_amd64.deb
