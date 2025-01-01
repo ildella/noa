82789ed6528337d045b41935c986ed4781d9c08f
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-01-01 19:42:42 -0300

    back to install debug debian, not appimage

--- cicd/debug-install.sh
+++ cicd/debug-install.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 VERSION=$(grep '^version =' src-tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
-# yarn desktop.build.deb -d
-# sudo dpkg -i src-tauri/target/debug/bundle/deb/NOA_"$VERSION"_amd64.deb
+yarn desktop.build.deb -d
+sudo dpkg -i src-tauri/target/debug/bundle/deb/NOA_"$VERSION"_amd64.deb
 
-yarn desktop.build.appimage -d
-./src-tauri/target/release/bundle/appimage/NOA_"$VERSION"_amd64.AppImage
+# yarn desktop.build.appimage -d
+# ./src-tauri/target/release/bundle/appimage/NOA_"$VERSION"_amd64.AppImage
