e3c78f3c8340fd2d39a7cd5dd11aecff3eeab29b
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-01-04 10:04:52 -0300

    uncomment the commit+push at the end of the script

--- bin/bump-version-cycle.sh
+++ bin/bump-version-cycle.sh
@@ -6,7 +6,7 @@ cargo release beta --manifest-path src-tauri/Cargo.toml --no-publish --no-tag --
 NEW_VERSION=$(grep '^version =' src-tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
 yarn version "$NEW_VERSION"
 yarn version "$NEW_VERSION"
-# git commit -am "Update version for release: $NEW_VERSION"
-# git push
+git commit -am "Update version for release: $NEW_VERSION"
+git push
 
 echo "New version: $NEW_VERSION"
