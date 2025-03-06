fb777f5922c22fb7bf8486b2edea5a7c8749f445
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-03-06 11:45:13 +0100

    removed src-tauri frm everywhere in the git repo

--- cicd/install-desktop.sh
+++ cicd/install-desktop.sh
@@ -5,7 +5,7 @@ set -uo pipefail
 target=${1:-'release'}
 # params if debug -> -d
 params=
-VERSION=$(grep '^version =' src-tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
+VERSION=$(grep '^version =' tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
 echo "Target: $target | version: $VERSION"
 # This handler is installed by `yarn tauri dev`. 
 # Removed to avoid collision with the installed debug version. 
@@ -13,4 +13,4 @@ rm ~/.local/share/applications/NOA-handler.desktop
 
 set -euo pipefail
 yarn desktop.build
-sudo dpkg -i "src-tauri/target/$target/bundle/deb/NOA_"$VERSION"_amd64.deb"
+sudo dpkg -i "tauri/target/$target/bundle/deb/NOA_"$VERSION"_amd64.deb"
