9974940aee58618b18a676855824234142667b8e
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-03-12 14:13:17 +0100

    fixed wrong path, ready to merge

--- bin/dev-install-debug.sh
+++ bin/dev-install-debug.sh
@@ -9,4 +9,4 @@ rm ~/.local/share/applications/NOA-handler.desktop
 set -euo pipefail
 VERSION=$(grep '^version =' tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
 yarn desktop.build.deb -d
-sudo dpkg -i src-tauri/target/debug/bundle/deb/NOA_"$VERSION"_amd64.deb
+sudo dpkg -i tauri/target/debug/bundle/deb/NOA_"$VERSION"_amd64.deb
