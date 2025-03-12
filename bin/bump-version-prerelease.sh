5d6cbe238e61540b91db8fcc7b008670c2b432fc
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-03-12 11:47:28 +0100

    tat cargo release with specific version - to avoid tauri windows build issue.......

--- bin/bump-version-prerelease.sh
+++ bin/bump-version-prerelease.sh
@@ -5,9 +5,8 @@ set -euo pipefail
 yarn version patch
 yarn version prerelease
 NEW_VERSION=$(< "package.json" jq -r .version)
-cargo release "$NEW_VERSION" --manifest-path tauri/Cargo.toml --no-publish --no-tag --no-push --no-confirm --execute
-
 git commit -am "Bump prerelease for new development cycle: $NEW_VERSION"
 git push
+cargo release "$NEW_VERSION" --manifest-path tauri/Cargo.toml --no-publish --no-tag --no-push --no-confirm --execute
 
 echo "New version: $NEW_VERSION"
