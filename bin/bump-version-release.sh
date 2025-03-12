81718d5af3781087cbfb10b403db77141b83f6ca
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-03-12 16:59:09 +0100

    release script now also tags and push to git

--- bin/bump-version-release.sh
+++ bin/bump-version-release.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-yarn version prerelease
+yarn version patch
 
 NEW_VERSION=$(< "package.json" jq -r .version)
 cd tauri
@@ -10,6 +10,11 @@ cargo set-version "$NEW_VERSION"
 cd ..
 
 git commit -am "Set release: $NEW_VERSION"
-# git push
+git push
+
+GIT_RELEASE_TAG=v"$NEW_VERSION"
+echo "Tag $GIT_RELEASE_TAG and push to repo."
+git tag "$GIT_RELEASE_TAG"
+git push --tags
 
 echo "New version: $NEW_VERSION"
