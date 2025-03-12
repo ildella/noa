aa7d056503a258728e3ee33fb5f23e583f2a65fc
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-03-12 16:52:26 +0100

    just aligned this script with the release one

--- bin/bump-version-prerelease.sh
+++ bin/bump-version-prerelease.sh
@@ -3,9 +3,13 @@
 set -euo pipefail
 
 yarn version prerelease
+
 NEW_VERSION=$(< "package.json" jq -r .version)
-cd tauri && cargo set-version "$NEW_VERSION"
-git commit -am "Bump prerelease for new development cycle: $NEW_VERSION"
+cd tauri
+cargo set-version "$NEW_VERSION"
+cd ..
+
+git commit -am "Bump prerelease to $NEW_VERSION"
 # git push
 
 echo "New version: $NEW_VERSION"
