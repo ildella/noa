02c5d8cbe6666e73afc1481d8a2583c0db69070e
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-03-12 14:09:37 +0100

    polished install debug version for dev

--- bin/dev-install-debug.sh
+++ bin/dev-install-debug.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 # This handler is installed by `yarn tauri dev`. 
 # Removed to avoid collision with the installed debug version. 
 rm ~/.local/share/applications/NOA-handler.desktop
 
-VERSION=$(grep '^version =' src-tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
+set -euo pipefail
+VERSION=$(grep '^version =' tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
 yarn desktop.build.deb -d
 sudo dpkg -i src-tauri/target/debug/bundle/deb/NOA_"$VERSION"_amd64.deb
