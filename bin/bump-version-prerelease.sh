18ad577ff848023b7767bb12f492341f52847694
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-03-12 16:21:39 +0100

    using cargo set-version in place of release, which is for other use cases. This prevent cargo to git tag and push every time, so we can do that manually and let package.json drive

--- bin/bump-version-prerelease.sh
+++ bin/bump-version-prerelease.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 yarn version prerelease
 NEW_VERSION=$(< "package.json" jq -r .version)
+cd tauri && cargo set-version "$NEW_VERSION"
 git commit -am "Bump prerelease for new development cycle: $NEW_VERSION"
-git push
-cargo release "$NEW_VERSION" --manifest-path tauri/Cargo.toml --no-publish --no-tag --no-push --no-confirm --execute
+# git push
 
 echo "New version: $NEW_VERSION"
--- bin/bump-version-release.sh
+++ bin/bump-version-release.sh
@@ -2,27 +2,14 @@
 
 set -euo pipefail
 
-CURRENT_VERSION=$(grep '^version =' tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
-echo "Releasing from: $CURRENT_VERSION"
+yarn version prerelease
 
-cargo release release --manifest-path tauri/Cargo.toml --no-publish --no-tag --no-push --no-confirm --execute
-RELEASED_VERSION=$(grep '^version =' tauri/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
-echo "Bump package.json -> $RELEASED_VERSION"
-yarn version "$RELEASED_VERSION"
-## LOL, I know. But it messes up if I run it only once.
-yarn version "$RELEASED_VERSION"
-git commit -am "Update version for release: $RELEASED_VERSION"
-git push
+NEW_VERSION=$(< "package.json" jq -r .version)
+cd tauri
+cargo set-version "$NEW_VERSION"
+cd ..
 
-GITHUB_RELEASE_TAG=v"$RELEASED_VERSION"
-echo "Tag $GITHUB_RELEASE_TAG and push to repo."
-git tag "$GITHUB_RELEASE_TAG"
-git push --tags
+git commit -am "Set release: $NEW_VERSION"
+# git push
 
-echo "
-  Release $GITHUB_RELEASE_TAG is ready.
-
-  To create a GitHub release and trigger the bundle build:
-
-    $ gh release create $GITHUB_RELEASE_TAG --generate-notes
-"
+echo "New version: $NEW_VERSION"
